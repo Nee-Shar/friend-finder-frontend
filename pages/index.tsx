58c2574c42bed16a5580fae6ba06095538d5e847
Neeraj Sharma <99169026+Nee-Shar@users.noreply.github.com>
2023-06-07 06:42:08 +0000

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,37 @@
+import { useState } from 'react';
 import { useSelector } from 'react-redux';
 import type { StoreType } from '../redux/store_type';
+import Login from "./login.tsx";
+import Logout from "./logout.tsx";
+import Signup from "./signup.tsx";
 
 const HomePage = () => {
   const loggedIn = useSelector((state: StoreType) => state.auth.loggedIn);
+  const [showSignup, setShowSignup] = useState(false);
+  const [showLogin, setShowLogin] = useState(false);
+
+  const renderAuthenticationButtons = () => {
+    if (loggedIn) {
+      return <Logout />;
+    } else {
+      return (
+        <div>
+          {!showSignup && (
+            <button onClick={() => setShowSignup(true)}>Sign Up</button>
+          )}
+          {!showLogin && (
+            <button onClick={() => setShowLogin(true)}>Login</button>
+          )}
+        </div>
+      );
+    }
+  };
+
   return (
     <div>
-      {loggedIn ? (
-        <h1>Welcome, you are logged in!</h1>
-      ) : (
-        <h1>Please log in.</h1>
-      )}
+      {renderAuthenticationButtons()}
+      {showSignup && <Signup />}
+      {showLogin && <Login />}
     </div>
   );
 };
